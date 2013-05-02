f928ba94019de854dc935af915da82f0a74fde58
nalytools <nalytools@gmail.com>
2013-05-01 21:11:44 -0700

    Primera versión de MVC con Entities

---
+++ MVC_Ejemplo/Controllers/EquipoLaboratorioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Ejemplo.Models;
+
+namespace MVC_Ejemplo.Controllers
+{
+    public class EquipoLaboratorioController : Controller
+    {
+        EquipoLaboratorioDataContext equipo = new EquipoLaboratorioDataContext();
+        //
+        // GET: /EquipoLaboratorio/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EquipoLaboratorio()
+        {
+            ViewBag.Message = "Material de Laboratorio";
+
+            var equipoResultados = from Equipo in equipo.Equipos
+                                   select Equipo;
+
+            return View(equipoResultados);
+        }
+
+    }
+}
